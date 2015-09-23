d8e5122834135de6cb19dced520f33f103c417d6
yannickoo <github@yannick.md>
2015-09-23 21:35:14 +0200

    adds initial files

---
+++ README.md
@@ -0,0 +1,14 @@
+Gulp task PostCSS
+=================
+
+> A gulp task for PostCSS processing.
+
+## Usage
+```
+require('gulp-task-postcss')
+```
+
+## Contributing
+
+Pull requests and stars are always welcome. For bugs and feature requests, [please create an issue](https://github.com/wunderkraut/gulp-task-postcss/issues/new).
+But before doing anything, please read the [CONTRIBUTING.md](./CONTRIBUTING.md) guidelines.
---
+++ index.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var projectConfig = require('../../config');
+
+var gulp = require('gulp');
+var path = require('path');
+var defaults = require('lodash.defaults');
+var sourcemaps = require('gulp-sourcemaps');
+var notify = require('gulp-notify');
+var postcss = require('gulp-postcss');
+
+// PostCSS plugins
+var autoprefixer = require('autoprefixer');
+
+// Merge default & project config
+var defaultConfig = {
+  src: '/postcss/**/*.p.css',
+  dest: '/css',
+  processors: {
+    autoprefixer: {
+      browsers: ['last 2 versions']
+    }
+  },
+  notify: {
+    title: 'Wunderkraut',
+    message: 'PostCSS compiled.'
+  }
+};
+
+var config = defaults(projectConfig.stylesheets, defaultConfig);
+
+// Default task mapping.
+gulp.task('stylesheets', ['postcss-compile']);
+
+
+// PostCSS with sourcemaps.
+gulp.task('postcss-compile', function () {
+  var processors = [
+    autoprefixer(config.processors.autoprefixer),
+  ];
+
+  return gulp.src(projectConfig.basePath + config.src)
+    .pipe(sourcemaps.init())
+    .pipe(postcss(processors))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(projectConfig.basePath + config.dest))
+    .pipe(notify({
+      title: config.notify.title,
+      message: config.notify.message,
+      sound: false
+    }));
+});
