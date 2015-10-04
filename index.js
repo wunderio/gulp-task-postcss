a0a16925a287bb0e99410c832374bd1f3ccf768d
Benedikt Rötsch <info@benedikt-roetsch.de>
2015-10-04 18:32:06 +0200

    wraps gulp-postcss into a stream object to show errors in cli and as notification

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
 var path = require('path');
 var defaultsDeep = require('lodash.defaultsdeep');
 var map = require('lodash.map');
+var notifier = require('node-notifier');
+
 var sourcemaps = require('gulp-sourcemaps');
-var notify = require('gulp-notify');
 var postcss = require('gulp-postcss');
 var rename = require('gulp-rename');
 var filter = require('gulp-filter');
@@ -46,22 +47,55 @@ module.exports = function (gulp, gulpConfig) {
       return require(processor)(config.processors[processor]);
     });
 
+    var errorThrown = false;
+
+    // Create stream to catch postcss errors.
+    var postcssStream = postcss(processors);
+
+    postcssStream.on('error', function (error) {
+      // Log error to console.
+      console.error(error.message);
+
+      // Display error notification.
+      var message = error.message
+        .replace(/^\/[^ ]+\//, '')
+        .replace(/\^/, '')
+        .replace(/\s/, ' ')
+        .trim();
+
+      notifier.notify({
+        title: config.notify.title + ' - PostCSS Error',
+        message: message,
+        icon: gulpConfig.notify.errorIcon,
+        sound: false
+      });
+
+      errorThrown = true;
+
+      this.emit('end');
+    });
+
     return gulp.src(path.join(gulpConfig.basePath, config.src))
       .pipe(filter(function (file) {
         return !/^_/.test(path.basename(file.path));
       }))
       .pipe(sourcemaps.init())
-      .pipe(postcss(processors))
+      .pipe(postcssStream)
       .pipe(sourcemaps.write())
       .pipe(rename(function (path) {
         // Remove ".p" from filename.
         path.basename = path.basename.substr(0, path.basename.length -2);
       }))
       .pipe(gulp.dest(path.join(gulpConfig.basePath, config.dest)))
-      .pipe(notify({
-        title: config.notify.title,
-        message: config.notify.message,
-        sound: false
-      }));
+      .on('end', function () {
+        if (!errorThrown) {
+          notifier.notify({
+            title: config.notify.title,
+            message: config.notify.message,
+            icon: gulpConfig.notify.successIcon,
+            sound: false
+          });
+        }
+      });
   });
 };
