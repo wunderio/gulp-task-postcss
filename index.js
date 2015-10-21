235985d4918518d603d3a2aeec5402a091987364
Benedikt Rötsch <info@benedikt-roetsch.de>
2015-10-22 00:18:14 +0200

    watch task now only processses changes files - fixes #4

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var postcssImport = require('postcss-import');
 
 module.exports = function (gulp, gulpConfig) {
 
-  gulpConfig = gulpConfig || { basePath: '.' };
+  gulpConfig = gulpConfig || {basePath: '.'};
 
   // Merge default config with gulp config.
   var defaultConfig = {
@@ -36,42 +36,41 @@ module.exports = function (gulp, gulpConfig) {
   };
 
   var config = defaultsDeep(gulpConfig, defaultConfig).stylesheets;
+  var streamSrc = path.join(gulpConfig.basePath, config.src);
+  var streamDest = path.join(gulpConfig.basePath, config.dest);
 
-  // Default watch task.
-  gulp.task('postcss-watch', ['postcss'], function () {
-    gulp.watch(path.join(gulpConfig.basePath, config.src), ['postcss'])
+  // Create postcss processors array based on the given configuration.
+  var processors = map(Object.keys(config.processors), function (processor) {
+    return require(processor)(config.processors[processor]);
   });
 
-  // PostCSS with sourcemaps.
-  gulp.task('postcss', function () {
-    var processors = map(Object.keys(config.processors), function (processor) {
-      return require(processor)(config.processors[processor]);
+  // Postcss error handler.
+  var errorThrown = false;
+  var postcssErrorHandler = function (error) {
+    // Log error to console.
+    console.error(error.message);
+
+    // Display error notification.
+    var message = error.message
+      .replace(/^\/[^ ]+\//, '')
+      .replace(/\^/, '')
+      .replace(/\s/, ' ')
+      .trim();
+
+    notifier.notify({
+      title: config.notify.title + ' - PostCSS Error',
+      message: message,
+      icon: gulpConfig.notify.errorIcon,
+      sound: false
     });
 
-    var errorThrown = false;
-    var postcssErrorHandler = function (error) {
-      // Log error to console.
-      console.error(error.message);
+    errorThrown = true;
 
-      // Display error notification.
-      var message = error.message
-        .replace(/^\/[^ ]+\//, '')
-        .replace(/\^/, '')
-        .replace(/\s/, ' ')
-        .trim();
+    this.emit('end');
+  };
 
-      notifier.notify({
-        title: config.notify.title + ' - PostCSS Error',
-        message: message,
-        icon: gulpConfig.notify.errorIcon,
-        sound: false
-      });
-
-      errorThrown = true;
-
-      this.emit('end');
-    };
-
+  // Creates core postcss stream used by both tasks.
+  var postcssGulpStream = function (srcPath) {
     // Wrap postcss in a stream to catch postcss errors.
     var postcssStream = postcss(processors);
 
@@ -88,12 +87,12 @@ module.exports = function (gulp, gulpConfig) {
     postcssStream.on('error', postcssErrorHandler);
     postcssImportStream.on('error', postcssErrorHandler);
 
-    return gulp.src(path.join(gulpConfig.basePath, config.src))
+    var stream = gulp.src(srcPath)
       .pipe(filter(function (file) {
         return !/^_/.test(path.basename(file.path));
       }))
       .pipe(sourcemaps.init())
-      .pipe(tap(function(file) {
+      .pipe(tap(function (file) {
         // Strips all single line comments from the base files.
         file.contents = new Buffer(stripSync(file.contents));
       }))
@@ -103,21 +102,40 @@ module.exports = function (gulp, gulpConfig) {
       .pipe(rename(function (path) {
         // Remove ".p" from filename if exists.
         var remove = '.p';
-        
-        if (path.basename.substr(path.basename.length - remove.length) == remove) {
+        if (path.basename.substr(path.basename.length - remove.length) === remove) {
           path.basename = path.basename.substr(0, path.basename.length - remove.length);
         }
       }))
-      .pipe(gulp.dest(path.join(gulpConfig.basePath, config.dest)))
       .on('end', function () {
+        var relativePath = path.relative(streamSrc, srcPath);
         if (!errorThrown) {
           notifier.notify({
             title: config.notify.title,
-            message: config.notify.message,
+            message: config.notify.message + "\n" + relativePath,
             icon: gulpConfig.notify.successIcon,
             sound: false
           });
         }
-      });
+        else {
+          errorThrown = false;
+        }
+      })
+      .pipe(gulp.dest(streamDest));
+
+    return stream;
+  };
+
+  // Default watch task for postcss.
+  gulp.task('postcss-watch', ['postcss'], function () {
+    gulp.watch(streamSrc, function (event) {
+      if (['changed', 'added'].indexOf(event.type) !== -1) {
+        return postcssGulpStream(event.path);
+      }
+    });
+  });
+
+  // Processes postcss source files.
+  gulp.task('postcss', function () {
+    return postcssGulpStream(streamSrc);
   });
 };
